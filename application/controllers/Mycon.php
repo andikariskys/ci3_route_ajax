3b5f1e4c57e50480d9a6ce4c9faf2980dadfd4e5
andikariskys <andikarisky313@gmail.com>
2023-03-26 11:29:40 +0700

    configuration

---
+++ application/controllers/Mycon.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Mycon extends CI_Controller
+    {
+        public function index()
+        {
+            echo "Hello World";
+        }
+    }
+    
+
+?>
---
+++ application/models/MyMod.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Mymod extends CI_Model
+    {
+        public function get_data()
+        {
+            # code...
+        }
+    }
+    
+
+?>
